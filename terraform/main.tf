f2b96b41f7bb84ff7b376e195bfa22870a3da14c
HimanshuVK1 <himanshu1vishwakarma@gmail.com>
2026-05-06 20:08:26 +0530

    chore: fix terraform formatting to satisfy ci check

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,10 +22,10 @@ module "kms" {
 module "logging" {
   source = "./modules/logging"
 
-  project_name = var.project_name
-  environment  = var.environment
-  account_id   = local.account_id
-  kms_key_arn  = module.kms.logs_key_arn
+  project_name            = var.project_name
+  environment             = var.environment
+  account_id              = local.account_id
+  kms_key_arn             = module.kms.logs_key_arn
   s3_access_log_bucket_id = "temp-value" # Dummy value, will fix later.
 }
 
